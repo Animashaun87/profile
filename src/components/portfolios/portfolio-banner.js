b475fc0aa95e384d737713ae61bc477275630c55
animashaun87 <animashaunbidemi07@gmail.com>
2020-08-20 22:40:28 +0100

    created portfolio banner

---
+++ src/components/portfolios/portfolio-banner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Portfolio from "./portfolio";
+
+function PortfolioBanner() {
+  return (
+    <>
+      <div>
+        <h1>Nice</h1>
+      </div>
+    </>
+  );
+}
+
+export default PortfolioBanner;
